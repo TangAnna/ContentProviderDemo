c9dbac9922922a72bade56030b636ee1a5189233
TangAnna <atang@singlemountaintech.com>
2018-06-19 18:26:11 +0800

    创建项目

---
+++ app/src/main/java/com/example/tanganan/contentproviderdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tanganan.contentproviderdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * ContentProvider的使用
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
